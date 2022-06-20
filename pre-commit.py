5a2154bfcdcc3d5f3caf1478c92e79fef0f1ded2
birkof007 <bjoe@seznam.cz>
2022-06-20 13:31:42 +0200

    Add files via upload
    Example of Laravel pre-commit script.

---
+++ pre-commit.py
@@ -0,0 +1,17 @@
+#!C:\Users\101946\AppData\Local\Programs\Python\Python310\python.exe
+
+import sys
+import subprocess
+def main():
+    """
+    print("Number of arguments: %d", len(sys.argv))
+    print("Argument list: %s" % str(sys.argv))
+    print(sys.argv[1])
+    sys.exit(1)
+    """
+    subprocess.run(["pip", "freeze", ">", "python_requirements.txt"], shell=True)
+    subprocess.run(["git", "add", "python_requirements.txt"], shell=True)
+
+
+if __name__=="__main__":
+    main()
